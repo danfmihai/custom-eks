21c40ed06650b5d31aa29674f0904c4062b7587b
Mihai <danfmihai@hotmail.com>
2021-11-16 12:24:51 +0000

    added command for

--- variables.tf
+++ variables.tf
@@ -8,27 +8,27 @@ variable "subnets" {
   type = map(any)
   default = {
     sub-1 = {
-      az   = "us-east-1a"
+      az       = "us-east-1a"
       cidr_prv = "10.0.1.0/24"
       cidr_pub = "10.0.101.0/24"
     }
     sub-2 = {
-      az   = "us-east-1b"
+      az       = "us-east-1b"
       cidr_prv = "10.0.2.0/24"
       cidr_pub = "10.0.102.0/24"
     }
-    
+
   }
   description = "Defines subnets and availibility zones"
 }
 
-variable cidr_vpc {
+variable "cidr_vpc" {
   type        = string
   default     = "10.0.0.0/16"
   description = "Vpc cidr"
 }
 
-variable azs {
+variable "azs" {
   type        = list(string)
   default     = []
   description = "description"
