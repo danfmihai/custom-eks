81f45930b64bfb9577f1bffb75acdb0536db6da2
Mihai <danfmihai@hotmail.com>
2021-11-16 14:48:04 +0000

    removed deployment from main file

--- main.tf
+++ main.tf
@@ -53,20 +53,6 @@ module "vpc" {
 }
 
 # Create eks cluster
-data "aws_eks_cluster" "cluster" {
-  name = module.eks_cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 module "eks_cluster" {
   source = "terraform-aws-modules/eks/aws"
 
@@ -83,11 +69,11 @@ module "eks_cluster" {
       asg_max_size  = 5
     },
     {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      name                 = "worker-group-1"
+      instance_type        = "t2.micro"
+      additional_userdata  = "echo foo bar"
+      asg_desired_capacity = 2
+      # additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     }
   ]
 
@@ -116,65 +102,3 @@ module "eks_cluster" {
 
 }
 
-# change context
-resource "null_resource" "java" {
-  depends_on = [module.eks_cluster]
-  provisioner "local-exec" {
-    command = "aws eks --region ${local.region}  update-kubeconfig --name $AWS_CLUSTER_NAME"
-    environment = {
-      AWS_CLUSTER_NAME = "${local.cluster_name}"
-    }
-  }
-}
-
-#create deployment
-resource "kubernetes_deployment" "java" {
-  metadata {
-    name = "microservice-deployment"
-    labels = {
-      app = "java-microservice"
-    }
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "java-microservice"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "java-microservice"
-        }
-      }
-      spec {
-        container {
-          image = "337204325105.dkr.ecr.us-east-1.amazonaws.com/java-app:latest"
-          name  = "java-microservice-container"
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
-
-#create service
-resource "kubernetes_service" "java" {
-  depends_on = [kubernetes_deployment.java]
-  metadata {
-    name = "java-microservice-service"
-  }
-  spec {
-    selector = {
-      app = "java-microservice"
-    }
-    port {
-      port        = 80
-      target_port = 8080
-    }
-    type = "LoadBalancer"
-  }
-}
